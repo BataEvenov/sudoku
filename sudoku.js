216905db5472346884c747bd30cf4ee1b60d9d59
Kirill <kirilltsypkin@gmail.com>
2024-07-26 16:27:16 +0300

    test

--- sudoku.js
+++ sudoku.js
@@ -1,19 +1,61 @@
 const fs = require('fs')
 //const puzzle = fs.readFileSync('./puzzle.txr', 'utf-8')
-function read() {
+function read(numSud) {
   const puzzle = fs.readFileSync('./puzzles.txt', 'utf-8');
   const regExFor81 = /([1-9-]{81})/gm;
   const arrayOfSudoku = puzzle.match(regExFor81)
-  const oneSudoku = arrayOfSudoku.shift()
+  const oneSudoku = arrayOfSudoku[0]
   // const regExFor9 = /([-]*[0-9][-]*){9}/
+  // const short = oneSudoku.slice(0, 10)
+
+//// map style
+  // const obosaniyMassiv = [];
+  // const littlearrays= arrayOfSudoku.map((el) => [el])
+  // const crazy = littlearrays.map((el, ) => el.map((el) => obosaniyMassiv.push(el.slice(0, 10))))
   
-  function slicer (oneSudoku){
-  const short = oneSudoku.slice(0, 8)
-console.log(short)
-  }
+///-------
+const array = []
+for(let i = 0 ; i < oneSudoku.length; i+=9){
+  
+    array.push(oneSudoku.slice(i, i+9))
+        
+      }
+      const arrrayOfArrays = array.map((el)=> [el])
+    
+      console.log(arrrayOfArrays)
+return arrrayOfArrays
+}
+//console.log(oneSudoku.slice(0,9))
+// console.log(array)
+
+
+
+
+//   for(let i = 0; i < oneSudoku.length; i++){
+//     if( i % 10 === 0){
+//  console.log(oneSudoku.join(''))
+//     }
+
+//   }
+  console.log()
+ 
  
   
-  }
+    
+  
+
+
+
+
+
+
+ 
+
+ 
+//   }
+ //console.log(short)
+  
+  
   //console.log(oneSudoku)
 
 //console.log(typeof oneSudoku)
